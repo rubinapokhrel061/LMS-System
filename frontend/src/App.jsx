ef6cf9de474538b18bde1acd133708e8979287bd
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2024-11-18 20:33:41 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-center text-3xl">
+      <Button variant="outline">check</Button>
+    </div>
+  );
+};
+
+export default App;
