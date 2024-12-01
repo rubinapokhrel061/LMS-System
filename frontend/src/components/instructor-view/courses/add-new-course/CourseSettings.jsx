8358e0720282cf39607345e5430e03cf177393a6
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2024-12-01 16:55:44 +0545

    courseSettingSlice

--- frontend/src/components/instructor-view/courses/add-new-course/CourseSettings.jsx
+++ frontend/src/components/instructor-view/courses/add-new-course/CourseSettings.jsx
@@ -1,12 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { APIForMedia } from "@/http";
-import React, { useState } from "react";
+import {
+  setImage,
+  setIsUploading,
+} from "@/./store/instructorSlice/courseSettingSlice";
 
 const CourseSettings = () => {
-  const [image, setImage] = useState({ image: "" });
-  const [isUploading, setIsUploading] = useState(false);
+  const dispatch = useDispatch();
+  const { formData, isUploading } = useSelector((state) => state.courseLanding);
+
   const handleImageUploadChange = async (event) => {
     const selectedImage = event.target.files[0];
     if (selectedImage) {
@@ -14,41 +19,39 @@ const CourseSettings = () => {
       imageData.append("file", selectedImage);
 
       try {
-        setIsUploading(true);
+        dispatch(setIsUploading(true));
+
         const response = await APIForMedia.post("media/upload", imageData);
-        console.log(response);
         if (response.data.success) {
-          setImage({
-            image: response?.data?.data?.url,
-          });
-          setIsUploading(false);
+          dispatch(setImage(response.data.data.url)); // Update the image in Redux store
         }
       } catch (error) {
-        setIsUploading(false);
         console.error("Error uploading image:", error);
+      } finally {
+        dispatch(setIsUploading(false));
       }
     }
   };
-  console.log(image);
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Course Setting</CardTitle>
       </CardHeader>
       <CardContent>
-        {image?.image ? (
-          <img src={image?.image} />
+        {formData.image ? (
+          <img src={formData.image} alt="Uploaded course image" />
         ) : (
           <div className="flex flex-col gap-3">
             <Label>Upload Course Image:</Label>
             <div>
-              {image.image ? (
+              {formData.image ? (
                 <div className="text-green-500 text-xs">Image Uploaded</div>
               ) : isUploading ? (
-                <div className="text-blue-500 text-xs">Uploading video...</div>
+                <div className="text-blue-500 text-xs">Uploading image...</div>
               ) : (
                 <div className="text-red-500 text-xs">
-                  Please select a image to upload
+                  Please select an image to upload
                 </div>
               )}
               <Input
